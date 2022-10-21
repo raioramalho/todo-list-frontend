e2ec69a977d580ef86a1c467619c517f343c4c8b
Alan Ramalho <ramalho.sit@gmail.com>
2022-10-21 15:12:35 -0300

    Working with CRUD API

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,19 +1,13 @@
-import data from '../gear/Api';
-import Card from './Card';
+import data from "../gear/Api"
+import Card from "./Card"
 
-function TaskList(props) {
-    const myArray = data
-    const newArray = []
-    myArray.forEach((el)=>{
-      newArray.push(
-          <Card id={el.id} taskId={el.id} taskTitle={el.title}/>
-      )
-    })
-    return (
-      <div id="content">
-        {newArray}
-      </div>
-    )
+function TaskList() {
+  const myArray = data
+  const newArray = []
+  myArray.forEach((el) => {
+    newArray.push(<Card id={el.id} taskId={el.id} taskTitle={el.title} />)
+  })
+  return <div id="content">{newArray}</div>
 }
 
 export default TaskList
--- src/gear/Api.jsx
+++ src/gear/Api.jsx
@@ -1,14 +1,17 @@
-const url = 'http://localhost:3000/tasks.json'
+const url = "http://localhost:3000/tasks"
 
 const api = async (method, body) => {
-  const req = { headers: new Headers({ "Content-Type": "application/json" }), method: "GET", mode: "cors"};
-  req.method = method;
-  if (method !== "GET") req.body = json(body);
-  const data = await (await fetch(url, req)).json();
-  return data;
+  const req = {
+    headers: new Headers({ "Content-Type": "application/json" }),
+    method: "GET",
+    mode: "cors",
+  }
+  req.method = method
+  if (method !== "GET") req.body = json(body)
+  const data = await (await fetch(url, req)).json()
+  return data
 }
 
 const data = await api("GET")
-
 
 export default data
