305bb082d26f0d00fd73c1896c39abd1dbee14f2
Alan Ramalho <ramalho.sit@gmail.com>
2022-10-22 14:19:02 -0300

    UFA!!

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -8,81 +8,87 @@ import Card from "../components/Card"
 import "./styles/App.css"
 
 function App() {
-  const [texto, setTexto] = useState("TODO APP WITH REACT AND RAILS")
+  const texto = "TODO APP WITH REACT AND RAILS"
   const [items, setItems] = useState([])
 
   useEffect(async () => {
-    fetch(myGear.url)
-      .then((response) => response.json())
-      .then((json) => {
-        const upDate = json
-        setItems(upDate)
-      })
-  }, [texto])
+    const loadData = async () => {
+      fetch(myGear.url)
+        .then((response) => response.json())
+        .then((json) => {
+          //const upDate = json
+          console.log(json)
+          setItems(json)
+        })
+    }
+    loadData()
+  }, [])
 
-  function handleDelTask(id) {
-    const delTask = api.delete(myGear.url + "/" + id)
-    console.log("DELETING TASK: ", id)
+  function handleDelTask(itemId) {
+    //console.log("FUNCTION: DEL", itemId)
+    const sendTask = api.delete(myGear.url + "/" + String(itemId))
+    setItems((prevState) => prevState.filter((where) => where.id !== itemId))
   }
 
   return (
-    <section>
-      <div className="App">
-        <h2>{texto}</h2>
-        <Bar>
-          <Input
-            id="testFunc"
-            type="text"
-            placeholder="Digite sua próxima taréfa:"
-          />
-          <Button
-            id="addButton"
-            title="Adicionar"
-            onClick={() => {
-              //const title = document.getElementById("testFunc").value
-              const newTask = {
-                task: {
-                  title: document.getElementById("testFunc").value,
-                  done: false,
-                },
-              }
-              const sendTask = api.post(myGear.urlCreate, newTask)
-              setItems((prevState) => [...prevState, newTask.task])
-            }}
-          />
-        </Bar>
-        {items.map((item) => (
-          <Card id={item.id} key={item.id}>
-            <p>{item.title}</p>
-            <div id="control-box">
-              <Button
-                id="doneTaskBtn"
-                title="v"
-                onClick={() => {
-                  const done = { task: { done: "true" } }
-                  const doneTask = api.put(
-                    myGear.url + "/" + `${item.id}`,
-                    done
-                  )
-                }}
-              />
-              <Button
-                id="delTaskBtn"
-                title="x"
-                onClick={() => {
-                  const itemId = `${item.id}`
-                  //console.log("DEL", itemId)
-                  setItems((prevState) =>
-                    prevState.filter((where) => where.id !== `${item.id}`)
-                  )
-                  //handleDelTask(itemId)
-                }}
-              />
-            </div>
-          </Card>
-        ))}
-      </div>
-    </section>
+    <div className="App">
+      <React.StrictMode>
+        <section>
+          <h2>{texto}</h2>
+          <Bar>
+            <Input
+              id="testFunc"
+              type="text"
+              placeholder="Digite sua próxima taréfa:"
+            />
+            <Button
+              id="addButton"
+              title="Adicionar"
+              onClick={() => {
+                //const title = document.getElementById("testFunc").value
+                const newTask = {
+                  task: {
+                    id: null,
+                    title: document.getElementById("testFunc").value,
+                    done: false,
+                  },
+                }
+                const sendTask = api.post(myGear.urlCreate, newTask)
+                setItems((prevState) => [...prevState, newTask.task])
+              }}
+            />
+          </Bar>
+          {items.map((item) => (
+            <Card id={item.id} key={item.id}>
+              <p>{item.title}</p>
+              <div id="control-box">
+                <Button
+                  id="doneTaskBtn"
+                  title="v"
+                  onClick={() => {
+                    const done = { task: { done: "true" } }
+                    const doneTask = api.put(
+                      myGear.url + "/" + `${item.id}`,
+                      done
+                    )
+                    setItems((prevState) => prevState)
+                  }}
+                />
+                <Button
+                  id="delTaskBtn"
+                  title="x"
+                  onClick={() => {
+                    const itemId = item.id //sim o id tá certin
+                    console.log("BUTTON: DEL", itemId)
+                    handleDelTask(itemId)
+                  }}
+                />
+              </div>
+            </Card>
+          ))}
+        </section>
+      </React.StrictMode>
+    </div>
   )
 }
 
