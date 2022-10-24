78d5d8dd7e1657fde9c9577a39c77330cf351bb5
laconic-br <116593333+laconic-br@users.noreply.github.com>
2022-10-24 15:58:08 -0300

    can delete this function?

--- src/gear/Brain.jsx
+++ src/gear/Brain.jsx
@@ -1,9 +1,6 @@
 import api from "./Api"
 
 const myGear = {
-  funDigit: function () {
-    console.log("Inputing..")
-  },
   url: "http://localhost:3000/tasks",
   urlCreate: "http://localhost:3000/tasks/create/",
 }
