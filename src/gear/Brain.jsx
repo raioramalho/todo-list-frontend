295f88edc2390df60c542014ba3d12c354400507
Alan Ramalho <ramalho.sit@gmail.com>
2022-10-24 15:47:17 -0300

    continue

--- src/gear/Brain.jsx
+++ src/gear/Brain.jsx
@@ -1,17 +1,11 @@
 import api from "./Api"
 
 const myGear = {
-  funClick: function () {
-    const title = document.getElementById("testFunc").value
-    const newTask = { task: { title: title, done: false } }
-    handleAddTask(newTask)
-  },
   funDigit: function () {
     console.log("Inputing..")
   },
   url: "http://localhost:3000/tasks",
   urlCreate: "http://localhost:3000/tasks/create/",
 }
-
 
 export default myGear
